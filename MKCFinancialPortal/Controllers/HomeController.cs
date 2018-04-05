edb56bcabd5845dbd2784c49210d34d894a9172d
Meghan <meghankcombs@gmail.com>
2018-04-05 15:36:41 -0400

    Commit from VS

--- MKCFinancialPortal/Controllers/HomeController.cs
+++ MKCFinancialPortal/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using MKCFinancialPortal.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,16 +20,47 @@ namespace MKCFinancialPortal.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            var emailBody = new StringBuilder();
+            emailBody.AppendLine(model.Body);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email from: <bold>{0}</bold>({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = "Financial Portal<meghankcombs@gmail.com>";
+                    model.Body = emailBody.ToString();
+
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = "Financial Portal Email",
+                        Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+                    var svc = new PersonalEmail();
+                    await svc.SendAsync(email);
+                    TempData["Message"] = "Your message has been sent!";
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+            }
+            return View(model);
         }
     }
 }
